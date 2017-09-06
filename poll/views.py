45c3b3c6799ff86acbbe4649a1ee0d1e850f6449
An-Death <saenarayoursorrow@gmail.com>
2017-09-06 14:27:48 +0300

    Migrate tutorial models

--- poll/views.py
+++ poll/views.py
@@ -5,4 +5,4 @@ from  django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('This is fucking shit! Take it and don`t cry like a bitch!' )
+    return HttpResponse('<head>shit</head><body><h1>This is fucking shit! Take it and don`t cry like a bitch!</h1></body>')
