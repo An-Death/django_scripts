483caeeaf5abe1e084f66e11a1ac19aa4d4f97d1
An-Death <saenarayoursorrow@gmail.com>
2017-09-07 15:16:01 +0300

    added some views

--- poll/urls.py
+++ poll/urls.py
@@ -3,5 +3,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex: /poll/
     url(r'^$', views.index, name='index'),
+    #  ex: /poll/5/
+    url(r'^(?P<question_id>[0-9]+)/*$', views.detail, name='detail'),
+    #  ex /poll/5/results
+    url(r'^(?P<question_id>[0-9]+)/result(s*)/$', views.results, name='results'),
+    # ex /poll/123123/vote
+    url(r'^(?P<question_id>[0-9]+)/vote(s*)/$', views.vote, name='vote')
+
 ]
--- poll/views.py
+++ poll/views.py
@@ -3,6 +3,22 @@ from  django.http import HttpResponse
 
 
 # Create your views here.
+htmlformat = '<head><title>BIG NASTY SHIT!</title></head><body><h1>{}</h1></body>'
+
 
 def index(request):
-    return HttpResponse('<head>shit</head><body><h1>This is fucking shit! Take it and don`t cry like a bitch!</h1></body>')
+    text = 'This is fucking shit! Take it and don`t cry like a bitch!'
+    return HttpResponse(htmlformat.format(text))
+
+def detail(request, question_id):
+    text = htmlformat.format('I have no idea what the shit is it "{}"! Sorry!'.format(question_id))
+    return HttpResponse(text)
+
+def results(request, question_id):
+    text = htmlformat.format('That second shit "{}" what i`w doesnt`t know  what is it -_-'.format(question_id))
+    return HttpResponse(text)
+
+def vote(request, question_id):
+    text = htmlformat.format('Just some text, OK? and that shit "{}" Any one can told me what that is it finaly ?!'.
+                             format(question_id))
+    return HttpResponse(text)
